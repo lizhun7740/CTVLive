fa455831950c909214f11c5e3c07ab0d4204f775
FRANKASEE <149130864+FRANKASEE@users.noreply.github.com>
2024-08-23 15:02:31 +0800

    Update config.py

--- config.py
+++ config.py
@@ -27,7 +27,7 @@ url_blacklist = [
 
 announcements = [
     {
-        "channel": "直播",
+        "channel": "定制直播",
         "entries": [
             {"name":"雷蒙影视直播","url":"https://cors.isteed.cc/https://raw.githubusercontent.com/n3rddd/N3RD/master/JN/N3RD/W/CTVThemeSong1.mp4","logo":"https://cors.isteed.cc/https://raw.githubusercontent.com/n3rddd/N3RD/master/JN/N3RD/W/ICON1.png"},
             {"name":"CrimeTV LIVE","url":"https://cors.isteed.cc/https://raw.githubusercontent.com/n3rddd/N3RD/master/JN/N3RD/W/CTVThemeSong2.mp4","logo":"https://cors.isteed.cc/https://raw.githubusercontent.com/n3rddd/N3RD/master/JN/N3RD/W/ICON2.png"},
