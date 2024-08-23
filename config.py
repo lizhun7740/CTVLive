3b4f874b0fc7186f2922cbb6d6d2e6faf19d60f5
FRANKASEE <149130864+FRANKASEE@users.noreply.github.com>
2024-08-24 00:16:39 +0800

    Update config.py

--- config.py
+++ config.py
@@ -14,7 +14,6 @@ source_urls = [
     "https://raw.githubusercontent.com/hus888yu/app/main/543.m3u"， #ADDED BY LEM ON 13/08/2024
     "http://175.178.251.183:6689/channel.txt",
     "https://live.fanmingming.com/tv/m3u/ipv6.m3u",
-    "https://raw.githubusercontent.com/yuanzl77/IPTV/main/直播/央视频道.txt",
     "http://120.79.4.185/new/mdlive.txt",
     "https://raw.githubusercontent.com/Fairy8o/IPTV/main/PDX-V4.txt",
     "https://raw.githubusercontent.com/Fairy8o/IPTV/main/PDX-V6.txt",
@@ -22,6 +21,31 @@ source_urls = [
     "https://tv.youdu.fan:666/live/",
     "http://ww.weidonglong.com/dsj.txt",
     "http://xhztv.top/zbc.txt",
+    "https://raw.githubusercontent.com/mlvjfchen/TV/main/iptv_list.txt",
+    "https://raw.githubusercontent.com/qingwen07/awesome-iptv/main/tvbox_live_all.txt",
+    "https://v.nxog.top/m/tv/1/",
+    "https://raw.githubusercontent.com/Guovin/TV/gd/result.txt",
+    "http://home.jundie.top:81/Cat/tv/live.txt",
+    "https://raw.githubusercontent.com/vbskycn/iptv/master/tv/hd.txt",
+    "https://cdn.jsdelivr.net/gh/YueChan/live@main/IPTV.m3u",
+    "https://raw.githubusercontent.com/cymz6/AutoIPTV-Hotel/main/lives.txt",
+    "https://raw.githubusercontent.com/PizazzGY/TVBox_warehouse/main/live.txt",
+    "https://fm1077.serv00.net/SmartTV.m3u",
+    "https://raw.githubusercontent.com/ssili126/tv/main/itvlist.txt",
+    "https://raw.githubusercontent.com/kimwang1978/collect-tv-txt/main/merged_output.txt"， #ADDED BY LEM ON 29/07/2024
+    "https://raw.githubusercontent.com/Supprise0901/TVBox_live/main/live.txt"， #ADDED BY LEM ON 29/07/2024
+    "https://raw.githubusercontent.com/yoursmile66/TVBox/main/live.txt"， #ADDED BY LEM ON 29/07/2024
+    "http://ttkx.live:55/lib/kx2024.txt"， #ADDED BY LEM ON 29/07/2024
+    "https://raw.githubusercontent.com/Kimentanm/aptv/master/m3u/iptv.m3u"， #ADDED BY LEM ON 29/07/2024
+    "https://raw.githubusercontent.com/Love4vn/love4vn/main/Sport.m3u"， #奥运 ON 29/07/2024
+    "https://cdn.jsdelivr.net/gh/joevess/IPTV@main/sources/iptv_sources.m3u8",
+    "https://cdn.jsdelivr.net/gh/joevess/IPTV@main/sources/home_sources.m3u8",
+    "https://cdn.jsdelivr.net/gh/joevess/IPTV@main/iptv.m3u8",
+    "https://cdn.jsdelivr.net/gh/ssili126/tv@main/itvlist.txt",
+    "https://cdn.jsdelivr.net/gh/YueChan/Live@main/IPTV.m3u",
+    "https://cdn.jsdelivr.net/gh/dxawi/0@main/tvlive.txt",
+    "https://cdn.jsdelivr.net/gh/XiaoZhang5656/xiaozhang-5656.github.io@main/iptv-live.txt",
+    "https://cdn.jsdelivr.net/gh/shidahuilang/shuyuan@shuyuan/iptv.txt"，
 ]
 
 url_blacklist = [
