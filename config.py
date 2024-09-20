9390c94738f4f0fe078fbde99236fad2e9a6c3e7
FRANKASEE <149130864+FRANKASEE@users.noreply.github.com>
2024-09-20 23:58:44 +0800

    Update config.py

--- config.py
+++ config.py
@@ -5,11 +5,6 @@ source_urls = [
     "https://m3u.hackserver.net/txt/fmml_dv6.txt",
     "https://raw.githubusercontent.com/frank11311/TV/refs/heads/master/result.txt",
     "https://raw.githubusercontent.com/FRANKASEE/TV/refs/heads/master/result.txt",
-    "https://raw.githubusercontent.com/FRANKASEE/iTV/refs/heads/main/iptv_list.txt",
-    "https://raw.githubusercontent.com/FRANKASEE/tviptv/refs/heads/Files/IPTVlive.m3u",
-    "https://raw.githubusercontent.com/FRANKASEE/iptv--/refs/heads/main/speedtest/zubo_fofa.txt",
-    "https://raw.githubusercontent.com/FRANKASEE/live-1/refs/heads/main/online.txt",
-    "https://raw.githubusercontent.com/FRANKASEE/live-1/refs/heads/main/iptv.txt",
     
 ]
 
