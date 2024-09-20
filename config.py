68640620bb4946b2d239d7c8cace7e05d8e57e9b
FRANKASEE <149130864+FRANKASEE@users.noreply.github.com>
2024-09-20 23:25:35 +0800

    Update config.py

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ source_urls = [
     "https://raw.githubusercontent.com/FRANKASEE/TV/refs/heads/master/result.txt",
     "https://raw.githubusercontent.com/FRANKASEE/iTV/refs/heads/main/iptv_list.txt",
     "https://raw.githubusercontent.com/FRANKASEE/tviptv/refs/heads/Files/IPTVlive.m3u",
-    "https://raw.githubusercontent.com/FRANKASEE/TV/refs/heads/master/result.txt",
+    "https://raw.githubusercontent.com/FRANKASEE/iptv--/refs/heads/main/speedtest/zubo_fofa.txt",
     
 ]
 
