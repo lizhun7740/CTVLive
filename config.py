65d24f463a1d213e9b8aa08ce5442a559fd0ae7b
FRANKASEE <149130864+FRANKASEE@users.noreply.github.com>
2024-09-24 09:42:01 +0800

    Update config.py

--- config.py
+++ config.py
@@ -19,12 +19,10 @@ source_urls = [
 url_blacklist = [
     "epg.pw/stream/",
     "103.40.13.71:12390",
-    "[2409:8087:1a01:df::4077]/PLTV/",
     "8.210.140.75:68",
     "154.12.50.54",
     "yinhe.live_hls.zte.com",
     "8.137.59.151",
-    "[2409:8087:7000:20:1000::22]:6060",
     "histar.zapi.us.kg",
     "www.tfiplaytv.vip",
     "dp.sxtv.top",
